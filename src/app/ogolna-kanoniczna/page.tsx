54d986f53f28f711001f795553a3f6c7579b177d
BozekMateusz <152381734+BozekMateusz@users.noreply.github.com>
2025-01-31 21:52:07 +0100

    Update page.tsx

--- src/app/ogolna-kanoniczna/page.tsx
+++ src/app/ogolna-kanoniczna/page.tsx
@@ -1,26 +1,7 @@
 "use client";
-import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function OgolnaKanoniczna() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setIsDarkMode(savedTheme === "dark");
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("theme", isDarkMode ? "light" : "dark");
-    document.body.classList.toggle("dark", isDarkMode);
-  }, [isDarkMode]);
-
   const examples = [
     {
       title: "Przykład 1",
@@ -30,7 +11,7 @@ export default function OgolnaKanoniczna() {
           <span>p = </span>
           <span className="flex flex-col items-center">
             <span className="ml-5">-(-4)</span>
-            <span className="ml-5 border-t-2 border-blue-700 dark:border-white w-full text-center">2 · 2</span>
+            <span className="ml-5 border-t-2 border-white w-full text-center">2 · 2</span>
           </span>
         </div>,
         "p = 1",
@@ -38,7 +19,7 @@ export default function OgolnaKanoniczna() {
           <span>q = </span>
           <span className="flex flex-col items-center">
             <span className="ml-5">1 - (-4)²</span>
-            <span className="ml-5 border-t-2 border-blue-700 dark:border-white w-full text-center">4 · 2</span>
+            <span className="ml-5 border-t-2 border-white w-full text-center">4 · 2</span>
           </span>
         </div>,
         "q = -1",
@@ -53,7 +34,7 @@ export default function OgolnaKanoniczna() {
           <span>p = </span>
           <span className="flex flex-col items-center">
             <span className="ml-5">-6</span>
-            <span className="ml-5 border-t-2 border-blue-700 dark:border-white w-full text-center">2 · -3</span>
+            <span className="ml-5 border-t-2 border-white w-full text-center">2 · -3</span>
           </span>
         </div>,
         "p = -1",
@@ -61,7 +42,7 @@ export default function OgolnaKanoniczna() {
           <span>q = </span>
           <span className="flex flex-col items-center">
             <span className="ml-5">-2 - 6²</span>
-            <span className="ml-5 border-t-2 border-blue-700 dark:border-white w-full text-center">4 · -3</span>
+            <span className="ml-5 border-t-2 border-white w-full text-center">4 · -3</span>
           </span>
         </div>,
         "q = 1",
@@ -76,7 +57,7 @@ export default function OgolnaKanoniczna() {
           <span>p = </span>
           <span className="flex flex-col items-center">
             <span className="ml-5">-4</span>
-            <span className="ml-5 border-t-2 border-blue-700 dark:border-white w-full text-center">2 · 1</span>
+            <span className="ml-5 border-t-2 border-white w-full text-center">2 · 1</span>
           </span>
         </div>,
         "p = -2",
@@ -84,7 +65,7 @@ export default function OgolnaKanoniczna() {
           <span>q = </span>
           <span className="flex flex-col items-center">
             <span className="ml-5">5 - 4²</span>
-            <span className="ml-5 border-t-2 border-blue-700 dark:border-white w-full text-center">4 · 1</span>
+            <span className="ml-5 border-t-2 border-white w-full text-center">4 · 1</span>
           </span>
         </div>,
         "q = 1",
@@ -94,11 +75,7 @@ export default function OgolnaKanoniczna() {
   ];
 
   return (
-    <div
-      className={`min-h-screen flex flex-col items-center py-10 px-4 transition-all duration-300 ${
-        isDarkMode ? "bg-gray-900 text-white" : "bg-gradient-to-br from-blue-50 to-blue-100"
-      }`}
-    >
+    <div className="min-h-screen flex flex-col items-center py-10 px-4 bg-gray-900 text-white transition-all duration-300">
       <header className="mb-10 text-center">
         <h1 className="text-5xl font-extrabold text-blue-800 drop-shadow-lg">
           <a
@@ -110,42 +87,27 @@ export default function OgolnaKanoniczna() {
         </h1>
       </header>
 
-      <button
-        onClick={toggleTheme}
-        className="absolute top-4 right-4 bg-gray-800 text-white p-2 rounded-md hover:bg-gray-700"
-      >
-        {isDarkMode ? "Jasny motyw" : "Ciemny motyw"}
-      </button>
-
       <main className="flex flex-col space-y-12 w-full max-w-5xl px-6">
-        <section
-          className={`bg-white dark:bg-slate-600 p-6 shadow-lg rounded-xl hover:scale-105 transform transition-transform duration-300 ${
-            isDarkMode ? "shadow-neutral-400" : "shadow-black"
-          }`}
-        >
-          <h2 className="text-2xl font-bold text-blue-700 dark:text-white">Wzory, których używamy:</h2>
-          <ul className="mt-4 text-lg text-blue-600  dark:text-white space-y-2">
+        <section className="bg-slate-600 p-6 shadow-lg rounded-xl hover:scale-105 transform transition-transform duration-300">
+          <h2 className="text-2xl font-bold text-white">Wzory, których używamy:</h2>
+          <ul className="mt-4 text-lg text-white space-y-2">
             <li>Postać ogólna: f(x) = ax² + bx + c</li>
             <li>Postać kanoniczna: f(x) = a(x - p)² + q</li>
-            <div>
-              <div className="flex flex-row">
+            <div className="flex flex-row">
                 <span className="mr-2">wzór na p:</span>
                 <li className="mt-3 mr-1">p = </li>
                 <li className="flex flex-col items-center">
                   <span className="">-b</span>
-                  <span className="w-14 border-t-2 border-blue-700 dark:border-white text-center">2a</span>
+                  <span className="w-14 border-t-2 border-white text-center">2a</span>
                 </li>
-              </div>
             </div>
-            <div>
-              <div className="flex flex-row">
+            <div className="flex flex-row">
                 <span className="mr-2">wzór na q:</span>
                 <li className="mt-3 mr-1">q = </li>
                 <li className="flex flex-col items-center">
                   <span className="ml-1">b² - 4ac</span>
-                  <span className="w-20 border-t-2 border-blue-700 dark:border-white text-center">4a</span>
+                  <span className="w-20 border-t-2 border-white text-center">4a</span>
                 </li>
-              </div>
             </div>
           </ul>
         </section>
@@ -153,15 +115,13 @@ export default function OgolnaKanoniczna() {
         {examples.map((example, index) => (
           <section
             key={index}
-            className={`bg-white dark:bg-slate-600 p-6 shadow-lg rounded-xl hover:scale-105 transform transition-transform duration-300 ${
-              isDarkMode ? "shadow-neutral-400" : "shadow-black"
-            }`}
+            className="bg-slate-600 p-6 shadow-lg rounded-xl hover:scale-105 transform transition-transform duration-300"
           >
-            <h2 className="text-2xl font-bold text-blue-700 dark:text-white">{example.title}:</h2>
-            <p className="mt-4 text-lg text-blue-600 dark:text-white">
+            <h2 className="text-2xl font-bold text-white">{example.title}:</h2>
+            <p className="mt-4 text-lg text-white">
               Dana jest funkcja w postaci ogólnej: {example.function}.
             </p>
-            <ol className="list-decimal mt-4 pl-6 text-lg text-blue-600 dark:text-white space-y-2">
+            <ol className="list-decimal mt-4 pl-6 text-lg text-white space-y-2">
               <li>
                 Oblicz współrzędną wierzchołka:
                 <ul className="list-disc pl-6">
